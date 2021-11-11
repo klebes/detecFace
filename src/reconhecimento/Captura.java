09a6ed8c431dadcf88ec8996005ac1bcbcfc496a
klebes <klebes-santos@blogdokdsti.com.br>
2021-11-11 11:55:47 -0200

    captura funcionando

--- src/reconhecimento/Captura.java
+++ src/reconhecimento/Captura.java
@@ -10,7 +10,15 @@ import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.FrameGrabber;
 import org.bytedeco.javacv.OpenCVFrameConverter;
 import org.bytedeco.javacv.OpenCVFrameGrabber;
-
+import static org.bytedeco.opencv.global.opencv_imgproc.COLOR_BGRA2GRAY;
+import static org.bytedeco.opencv.global.opencv_imgproc.cvtColor;
+import static org.bytedeco.opencv.global.opencv_imgproc.rectangle;
+import org.bytedeco.opencv.opencv_core.Mat;
+import org.bytedeco.opencv.opencv_core.Rect;
+import org.bytedeco.opencv.opencv_core.RectVector;
+import org.bytedeco.opencv.opencv_core.Scalar;
+import org.bytedeco.opencv.opencv_core.Size;
+import org.bytedeco.opencv.opencv_objdetect.CascadeClassifier;
 /**
  *
  * @author KDS
@@ -18,17 +26,30 @@ import org.bytedeco.javacv.OpenCVFrameGrabber;
 public class Captura {
     public static void main(String[] args) throws FrameGrabber.Exception {
         KeyEvent press = null;
-        OpenCVFrameConverter.ToMat converterMat = new OpenCVFrameConverter.ToMat();
+        
+        OpenCVFrameConverter.ToMat convertMat= new OpenCVFrameConverter.ToMat();
         OpenCVFrameGrabber camera = new OpenCVFrameGrabber(0);
         camera.start();
-        CanvasFrame cFrame = new CanvasFrame("Preview", CanvasFrame.getDefaultGamma() / camera.getGamma());
-        Frame frameCapturado = null;
-        while ((frameCapturado = camera.grab()) != null) {            
-            if (cFrame.isVisible()) {
+       CascadeClassifier detectorFace = new CascadeClassifier("src/recursos/haarcascade_frontalface_alt.xml");
+        CanvasFrame cFrame = new CanvasFrame("Preview", CanvasFrame.getDefaultGamma()/camera.getGamma());
+        Frame frameCapturado =null;
+        Mat imagemColorida = new Mat();
+        while((frameCapturado = camera.grab()) != null){
+            imagemColorida = convertMat.convert(frameCapturado);
+            Mat imagemCinza = new Mat();
+            cvtColor(imagemColorida, imagemCinza, COLOR_BGRA2GRAY);
+            RectVector facesDetectadas = new RectVector();
+            detectorFace.detectMultiScale(imagemCinza, facesDetectadas, 1.1, 1, 0, new Size(150, 150), new Size(500, 500));
+            for(int i = 0; i < facesDetectadas.size(); i++){
+                Rect dadosFace = facesDetectadas.get(0);
+                rectangle(imagemColorida, dadosFace, new Scalar(0,0,255, 0));
+            }
+            if(cFrame.isVisible()){
                 cFrame.showImage(frameCapturado);
             }
-            cFrame.dispose();
-            camera.stop();
+        }
+        cFrame.dispose();
+        camera.stop();
         }
     }
-}
+
